4389c66384085b653607b97f1a0cb2a5a17577f5
vladk <vlad_krasovsky@yahoo.com>
2020-08-20 16:49:13 -0700

    Updates

---
+++ precache-manifest.c641f7af7fd3e7f93357a77732cafcae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905927e61a2d53377dc53d3198ba1ffa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc63069dcc790dfe2cef",
+    "url": "./static/css/main.1f6d1180.chunk.css"
+  },
+  {
+    "revision": "9ab6cd28f5f43cc8e09f",
+    "url": "./static/js/2.44d0c62c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.44d0c62c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc63069dcc790dfe2cef",
+    "url": "./static/js/main.40c1880a.chunk.js"
+  },
+  {
+    "revision": "8d23f8c67d350f728dbc",
+    "url": "./static/js/runtime-main.01054999.js"
+  }
+]);
